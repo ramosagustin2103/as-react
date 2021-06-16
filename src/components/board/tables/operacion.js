9444e672394e704e2f654413827ceeb6c39a9eb5
Mariano Valdez <marianovaldez92@gmail.com>
2021-06-16 16:12:26 -0300

    fixed document column in operacioens

--- src/components/board/tables/operacion.js
+++ src/components/board/tables/operacion.js
@@ -14,13 +14,20 @@ const TableOperacion = ({data, columns}) => {
   const tableHeaders = columns.map(c => ({ label: c.Header, key: typeof c.accessor === "string" ? c.accessor : c.Header.toLowerCase() }));
   const column =[
     {label: tableHeaders[0] !== undefined ?tableHeaders[0].label : " ", value:tableHeaders[0]!== undefined ?tableHeaders[0].key :" "},
-    {label: "Documento", value: (col) => col.documento.nombre},
+    // {label: "Documento", value: (col) => col.documento.nombre},
+    {label: tableHeaders[1] !== undefined ? tableHeaders[1].label : " ", value:tableHeaders[1] !== undefined ?tableHeaders[2].key :" "},
     {label: tableHeaders[2] !== undefined ? tableHeaders[2].label : " ", value:tableHeaders[2] !== undefined ?tableHeaders[2].key :" "},
     {label: tableHeaders[3] !== undefined ? tableHeaders[3].label : " ", value:tableHeaders[3] !== undefined ?tableHeaders[3].key :" "},
     {label: tableHeaders[4] !== undefined ? tableHeaders[4].label: " ", value:tableHeaders[4] !== undefined ?tableHeaders[4].key :" "},
     {label: tableHeaders[5] !== undefined ? tableHeaders[5].label : " ", value:tableHeaders[5] !== undefined ?tableHeaders[5].key : " "},
     {label: tableHeaders[6] !== undefined ? tableHeaders[6].label : " ", value:tableHeaders[6] !== undefined ?tableHeaders[6].key : " "},
-    {label: tableHeaders[7] !== undefined ? tableHeaders[7].label : " ", value:tableHeaders[7] !== undefined ?tableHeaders[7].key : " "}]
+    {label: tableHeaders[7] !== undefined ? tableHeaders[7].label : " ", value:tableHeaders[7] !== undefined ?tableHeaders[7].key : " "},
+    {label: tableHeaders[8] !== undefined ? tableHeaders[8].label : " ", value:tableHeaders[8] !== undefined ?tableHeaders[8].key : " "},
+    {label: tableHeaders[9] !== undefined ? tableHeaders[9].label : " ", value:tableHeaders[9] !== undefined ?tableHeaders[9].key : " "},
+    {label: tableHeaders[10] !== undefined ? tableHeaders[10].label : " ", value:tableHeaders[10] !== undefined ?tableHeaders[10].key : " "},
+    {label: tableHeaders[11] !== undefined ? tableHeaders[11].label : " ", value:tableHeaders[11] !== undefined ?tableHeaders[11].key : " "},
+    {label: tableHeaders[12] !== undefined ? tableHeaders[12].label : " ", value:tableHeaders[12] !== undefined ?tableHeaders[12].key : " "},
+    {label: tableHeaders[13] !== undefined ? tableHeaders[13].label : " ", value:tableHeaders[13] !== undefined ?tableHeaders[13].key : " "}]
 
   const refButton = useRef(null);
   const columns_final = columns.map(c => {
